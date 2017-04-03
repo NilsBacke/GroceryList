00ebcbde603cf06768e65ef2a9d5266f63094a91
sharonday <daya@sharonim.com>
2017-04-03 11:05:27 -0400

    made the health logic class, nothing actually in there yet

---
+++ app/src/main/java/com/example/nils/grocerylist/HealthLogic.java
@@ -0,0 +1,11 @@
+package com.example.nils.grocerylist;
+
+/**
+ * Created by Daya on 4/3/2017.
+ */
+
+public class HealthLogic {
+    public int oseIngerdients(Item q){
+        return 1;
+    }
+}
