7b218fbd184faf099c88189ab34d4bee20e869da
sharonday <daya@sharonim.com>
2017-04-03 11:14:12 -0400

    health logic update

--- app/src/main/java/com/example/nils/grocerylist/HealthLogic.java
+++ app/src/main/java/com/example/nils/grocerylist/HealthLogic.java
@@ -5,7 +5,23 @@ package com.example.nils.grocerylist;
  */
 
 public class HealthLogic {
+
+    Item a;
+    Item b;
+    int apoints = 0;
+    int bpoints = 0;
+
+    public HealthLogic(Item y, Item z){
+        a = y;
+        b = z;
+    }
+
     public int oseIngerdients(Item q){
         return 1;
     }
+    //compare sugar content
+    //compare calories
+    //compare fat
+    //compare protein
+    //compare ingredient numnbers
 }
