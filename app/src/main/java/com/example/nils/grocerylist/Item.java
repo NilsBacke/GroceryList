ff07dce35627ece8ff30bc7dfd151e5589982607
patri <patrickrwilcox@gmail.com>
2017-04-13 10:08:55 -0400

    hllo

--- app/src/main/java/com/example/nils/grocerylist/Item.java
+++ app/src/main/java/com/example/nils/grocerylist/Item.java
@@ -8,14 +8,37 @@ import java.util.ArrayList;
  */
 
 public class Item implements Serializable {
-
+    /**
+     *
+     */
     private String name;
+
+    /**
+     *
+     */
     private Double price, ppu, fat, fiber, sugar, protein, fatCalories, cholesterol, sodium, carbs;
     private int calories;
     private int id;
     private String ingredients;
     public int points;
 
+    /**
+     * Constructor that initializes the item's id, name, price information, and nutritionial information.
+     * @param id ID number for each item.
+     * @param name Name of the item.
+     * @param price The price of the item in US dollars.
+     * @param ppu The price per unit of the item in US dollars.
+     * @param calories The total number of calories per serving for the item.
+     * @param fatCalories The number of calories from fat per serving for the item.
+     * @param fat The amount of fat per serving for the item in grams.
+     * @param cholesterol The amount of cholesterol per serving for the item in milligrams.
+     * @param sodium The amount of sodium per serving for the item in milligrams.
+     * @param carbs The amount of carbohydrates per serving for the item in grams.
+     * @param fiber The amount of fiber per serving for the item in grams.
+     * @param sugar The amount of sugar per serving for the item in grams.
+     * @param protein The amount of protein per serving for the item in grams.
+     * @param ingredients The list of ingredients for the item.
+     */
     public Item(int id, String name, Double price, Double ppu, int calories, Double fatCalories, Double fat, Double cholesterol,
                 Double sodium, Double carbs, Double fiber, Double sugar, Double protein, String ingredients) {
         this.id = id;
@@ -35,7 +58,9 @@ public class Item implements Serializable {
         points = 0;
     }
 
-
+    /**
+     * Default constructor.
+     */
     public Item() {
         this.name = "";
         this.price = 0.;
@@ -53,127 +78,246 @@ public class Item implements Serializable {
         points = 0;
     }
 
+    /**
+     * Gets the id of the item.
+     * @return The id of the item.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets the name of the item.
+     * @return The name of the item.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the price of the item.
+     * @return The price of the item.
+     */
     public Double getPrice() {
         return price;
     }
 
+    /**
+     * Gets the price per unit of the item.
+     * @return The price per unit of the item.
+     */
     public Double getPPU() {
         return ppu;
     }
 
+    /**
+     * Gets the number of calories for the item.
+     * @return The number of calories for the item.
+     */
     public int getCalories() {
         return calories;
     }
 
+    /**
+     * Gets the number of calories from fat for the item.
+     * @return The number of calories from fat for the item.
+     */
     public Double getFatCalories() {
         return fatCalories;
     }
 
+    /**
+     * Gets the amount of fat for the item.
+     * @return The amount of fat for the item.
+     */
     public Double getFat() {
         return fat;
     }
 
+    /**
+     * Gets the amount of cholesterol for the item.
+     * @return The amount cholesterol for the item.
+     */
     public Double getCholesterol() {
         return cholesterol;
     }
 
+    /**
+     * Gets the amount of sodium for the item.
+     * @return The amount of sodium the item.
+     */
     public Double getSodium() {
         return sodium;
     }
 
+    /**
+     * Gets the amount of carbohydrates for the item.
+     * @return The amount of carbohydrates for the item.
+     */
     public Double getCarbs() {
         return carbs;
     }
 
+    /**
+     * Gets the amount of fiber for the item.
+     * @return The amount of fiber for the item.
+     */
     public Double getFiber() {
         return fiber;
     }
 
+    /**
+     * Gets the amount of sugar for the item.
+     * @return The amount of sugar for the item.
+     */
     public Double getSugar() {
         return sugar;
     }
 
+    /**
+     * Gets the amount of protein for the item.
+     * @return The amount of protein for the item.
+     */
     public Double getProtein() {
         return protein;
     }
 
+    /**
+     * Gets the list of ingredients for the item.
+     * @return The list of ingredients for the item.
+     */
     public String[] getIngredients() {
         return ingredients.split(",");
     }
 
 
-
-
+    /**
+     * Sets the ID of the item to the value of id.
+     * @param id The new id of the item.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Sets the name of the item to name.
+     * @param name The new name of the item.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the price of the item to price.
+     * @param price The new price of the item.
+     */
     public void setPrice(Double price) {
         this.price = price;
     }
 
+    /**
+     * Sets the price per unit of the item to ppu.
+     * @param ppu The new price per unit of the item.
+     */
     public void setPPU(Double ppu) {
         this.ppu = ppu;
     }
 
+    /**
+     * Sets the number of calories for the item to calories.
+     * @param calories The new number of calories for the item.
+     */
     public void setCalories(int calories) {
         this.calories = calories;
     }
 
+    /**
+     * Sets the number of calories from fat for the item to fatCalories.
+     * @param fatCalories The new number of caloires from fat for the item
+     */
     public void setFatCalories(Double fatCalories) {
         this.fatCalories = fatCalories;
     }
 
+    /**
+     * Sets the amount of fat for the item to fat.
+     * @param fat The new amount of fat for the item
+     */
     public void setFat(Double fat) {
         this.fat = fat;
     }
 
+    /**
+     * Sets the amount of cholesterol for the item to cholesterol.
+     * @param cholesterol The new amount of cholesterol of the item
+     */
     public void setCholesterol(Double cholesterol) {
         this.cholesterol = cholesterol;
     }
 
+    /**
+     * Sets the amount of sodium for the item to sodium.
+     * @param sodium The new amount of sodium for the item
+     */
     public void setSodium(Double sodium) {
         this.sodium = sodium;
     }
 
+    /**
+     * Sets the amount of carbohydrates for the item to carbs.
+     * @param carbs The new amount of carbohydrates for the item
+     */
     public void setCarbs (Double carbs) {
         this.carbs = carbs;
     }
 
+    /**
+     * Sets the amount of fiber for the item to fiber.
+     * @param fiber The new amount of fiber for the item
+     */
     public void setFiber (Double fiber) {
         this.fiber = fiber;
     }
 
+    /**
+     * Sets the amount of sugar for the item to sugar.
+     * @param sugar The new amount of sugar for the item
+     */
     public void setSugar (Double sugar) {
         this.sugar = sugar;
     }
 
+    /**
+     * Sets the amount of protein for the item to protein.
+     * @param protein The new amount of protein of the item
+     */
     public void setProtein (Double protein) {
         this.protein = protein;
     }
 
+    /**
+     * Sets the list of ingredients for the item to ingredients.
+     * @param ingredients The new list of ingredients for the item
+     */
     public void setIngredients(String ingredients) {
         this.ingredients = ingredients;
     }
 
 
     @Override
+
+    /**
+     *
+     * @return name The name of the item.
+     */
     public String toString() {
         return name;
     }
 
+    /**
+     *
+     * @return
+     */
     public String pricetoString() {
         return ("$" + price);
     }
