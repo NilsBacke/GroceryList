091f9b75332628d3aa70e5e1edfbef8b08747785
NilsBacke <nils.backe@gmail.com>
2017-05-12 11:20:53 -0400

    final build 3 (with comments)

--- app/src/main/java/com/example/nils/grocerylist/AlternateItems/AlternateItemsActivity.java
+++ app/src/main/java/com/example/nils/grocerylist/AlternateItems/AlternateItemsActivity.java
@@ -1,9 +1,13 @@
 package com.example.nils.grocerylist.AlternateItems;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -125,8 +129,5 @@ public class AlternateItemsActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
-
-
-
 
 }
